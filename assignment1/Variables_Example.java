382fec8de7050c656456f69cd28b6db8365f41be
Yaswanth2391 <164739747+Yaswanth2391@users.noreply.github.com>
2024-03-29 11:35:18 +0530

    Add files via upload

---
+++ assignment1/Variables_Example.java
@@ -0,0 +1,14 @@
+
+class Variables_Example
+ {
+   public static void main(String args[])
+    {
+      byte Number1=40;
+      byte Number2=30;
+     
+
+    System.out.println("Result=Number1 and Number2 values are not equal");
+            
+                
+     }
+}
---
+++ assignment1/Variables_Example1.java
@@ -0,0 +1,21 @@
+
+class Variables_Example1
+ {
+   public static void main(String args[])
+    {
+         int Number1=20,Number2=30,Number3=10,Result;
+            
+                  Result=Number1+Number2+Number3;
+                 System.out.println("Addition Result="+Result);
+                 
+                  Result=Number1-Number2-Number3;
+                 System.out.println("Subtraction Result="+Result);
+
+                  Result=(Number1+Number2+Number3)/3;
+                 System.out.println("Average Result="+Result);
+
+                  Result=Number1*Number2*Number3;
+                 System.out.println("Multiplication Result="+Result);
+         
+     }
+}
